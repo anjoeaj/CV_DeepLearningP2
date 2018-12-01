e709ed00509f7b91346df8985445f4b2ff20a038
anjoeaj <anjoeaj@gmail.com>
2018-12-01 06:23:49 +0000

    Add files via upload

--- mnist.py
+++ mnist.py
@@ -6,9 +6,10 @@ import torch.nn.functional as F
 import torch.optim as optim
 from torchvision import datasets, transforms
 import matplotlib.pyplot as plt
+import numpy as np
 
 # Training hyperparameters
-epochs = 10
+epochs = 25
 batch_size = 64
 learning_rate = 0.01
 momentum = 0.5
@@ -81,15 +82,15 @@ class InceptionNet(nn.Module):
     def __init__(self):
         super(InceptionNet, self).__init__()
 
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=16,
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=24,
                                kernel_size=5)
 
         # create the second Convolution layer
-        self.conv2 = nn.Conv2d(in_channels=88, out_channels=24,
+        self.conv2 = nn.Conv2d(in_channels=88, out_channels=32,
                                kernel_size=5)
-        self.conv2_dropout = nn.Dropout2d(p = 0.5)
-        self.inception1 = InceptionA(input_channels = 16)
-        self.inception2 = InceptionA(input_channels = 24)
+        self.conv2_dropout = nn.Dropout2d(p = 0.15)
+        self.inception1 = InceptionA(input_channels = 24)
+        self.inception2 = InceptionA(input_channels = 32)
 
         self.maxpool = nn.MaxPool2d(2)
         self.fc = nn.Linear(1408, 10)
@@ -118,40 +119,42 @@ class InceptionNet(nn.Module):
 class CNN(nn.Module):
     def __init__(self):
         super(CNN, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=16,
-                               kernel_size=3, stride=1, padding=0)
-
-        # create the second Convolution layer
-        self.conv2 = nn.Conv2d(in_channels=16, out_channels=64,
+        self.bm = nn.BatchNorm2d(1)
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=128,
                                kernel_size=5, stride=1, padding=0)
 
-        # self.conv3 = nn.Conv2d(in_channels=64, out_channels=64,
-        #                        kernel_size=3, stride=1, padding=0)
+        # create the second Convolution layer
+        self.conv2 = nn.Conv2d(in_channels=128, out_channels=800,
+                               kernel_size=5, stride=1, padding=0)
+
+#        self.conv3 = nn.Conv2d(in_channels=512, out_channels=1024,
+#                                kernel_size=3, stride=1, padding=0)
 
         self.mPool = nn.MaxPool2d(2)
 
-        self.fc1 = nn.Linear(in_features=64, out_features=10)
-        self.fc2 = nn.Linear(in_features=32, out_features=24)
-        self.fc3 = nn.Linear(in_features=24, out_features=10)
+        self.fc1 = nn.Linear(in_features=80000, out_features=512)
+        self.fc1_dropout = nn.Dropout2d(p=0.45)
+        self.fc2 = nn.Linear(in_features=512, out_features=128)
+        self.fc3 = nn.Linear(in_features=128
+                             , out_features=10)
+        
+        self.batch_norm2 = nn.BatchNorm2d(128)
         nn.init.kaiming_normal_(self.conv1.weight)
         nn.init.kaiming_normal_(self.conv2.weight)
-        nn.init.kaiming_normal_(self.conv3.weight)
+#        nn.init.kaiming_normal_(self.conv3.weight)
         nn.init.kaiming_normal_(self.fc1.weight)
-        # nn.init.kaiming_normal_(self.fc2.weight)
-        # nn.init.kaiming_normal_(self.fc3.weight)
+        nn.init.kaiming_normal_(self.fc2.weight)
+        nn.init.kaiming_normal_(self.fc3.weight)
 
     def forward(self, x):
         input_size = x.size(0)
+        x = self.bm(x)
         x = self.conv1(x)
-        x = self.mPool(x)
+
         x = F.relu(x)
         # print(x.shape)
 
         x = self.conv2(x)
-        x = self.mPool(x)
-        x = F.relu(x)
-        # print(x.size())
-        x = self.conv3(x)
         x = self.mPool(x)
         x = F.relu(x)
 
@@ -159,8 +162,12 @@ class CNN(nn.Module):
 
         x = self.fc1(x)
         x = F.relu(x)
+        x = self.fc1_dropout(x)
+        
         x = self.fc2(x)
         x = F.relu(x)
+        x = self.fc1_dropout(x)
+#        x = self.batch_norm2(x)
         x = self.fc3(x)
         return F.log_softmax(x, dim=1)
 
@@ -208,7 +215,7 @@ def train(model, device, train_loader, optimizer, epoch, losses=[], counter=[], 
         data, target = data.to(device), target.to(device)
         optimizer.zero_grad()
         output = model(data)
-        loss = F.nll_loss(output, target)
+        loss = F.cross_entropy(output, target)
         loss.backward()
         optimizer.step()
         if batch_idx % log_interval == 0:
@@ -230,7 +237,7 @@ def test(model, device, test_loader, losses=[], errors=[]):
         for data, target in test_loader:
             data, target = data.to(device), target.to(device)
             output = model(data)
-            test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
+            test_loss += F.cross_entropy(output, target, reduction='sum').item()  # sum up batch loss
             pred = output.max(1, keepdim=True)[1]  # get the index of the max log-probability
             correct += pred.eq(target.view_as(pred)).sum().item()
 
@@ -240,6 +247,15 @@ def test(model, device, test_loader, losses=[], errors=[]):
         100. * correct / len(test_loader.dataset)))
     losses.append(test_loss)
     errors.append(100. * (1 - correct / len(test_loader.dataset)))
+
+def save_predictions(model, device, test_loader, path):
+    model.eval()
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = F.softmax(model(data), dim=1)
+            with open(path, "a") as out_file:
+                np.savetxt(out_file, output)
 
 
 def main():
@@ -271,10 +287,11 @@ def main():
     # model creation
     #model = FCN().to(device)
     model = CNN().to(device)
-    # model = InceptionNet().to(device)
+#    model = InceptionNet().to(device)
+#    model = CNNModel().to(device)
     # optimizer creation
-    # optimizer = optim.SGD(model.parameters(), lr=learning_rate, momentum=momentum, weight_decay=0.001)
-    optimizer = optim.Adam(model.parameters(), lr=learning_rate, weight_decay=0.001)
+    optimizer = optim.SGD(model.parameters(), lr=learning_rate, momentum=momentum, weight_decay=0.001)
+#    optimizer = optim.Adam(model.parameters(), lr=learning_rate, weight_decay=0.001)
 
     # lists for saving history
     train_losses = []
@@ -300,7 +317,11 @@ def main():
     # extract and plot random samples of data with predicted labels
     data, _, pred = predict_batch(model, device, test_loader)
     plot_data(data, pred, 'Predicted')
-
+    
+    #Save the model
+    torch.save(model.state_dict(), '2c3f.pt')
+    
+    save_predictions(model, device, test_loader, "E:\\predictions.txt")
 
 if __name__ == '__main__':
     main()
